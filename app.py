ee86a641c9ad7874abed9816694d16678400d284
mini3213 <summin7119@gmail.com>
2026-05-11 11:07:45 +0900

    docker 배포 추가

--- app.py
+++ app.py
@@ -226,4 +226,5 @@ def simulator_status():
     return jsonify({'running': running})
 
 if __name__ == '__main__':
-    app.run(debug=True, use_reloader=False)
+    port = int(os.getenv('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False, use_reloader=False)
